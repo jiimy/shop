78350ee7205df581309edae99dcfe9275f24a805
rudwnok <rudwnok123@naver.com>
2023-02-15 22:55:24 +0900

    라우터 설정

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from "react-router-dom";
 
 // const express = require('express');
 // const app = express();
@@ -22,9 +23,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
